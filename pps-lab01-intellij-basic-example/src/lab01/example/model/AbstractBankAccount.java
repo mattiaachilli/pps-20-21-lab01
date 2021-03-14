2ad6ebc86ed14b941fb6272b8d1914ddef795457
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-03-14 16:07:47 +0100

    Add AbstractBankAccount

---
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/AbstractBankAccount.java
@@ -0,0 +1,51 @@
+package lab01.example.model;
+
+public abstract class AbstractBankAccount implements BankAccount {
+
+    private double balance;
+    private final AccountHolder accountHolder;
+
+    public AbstractBankAccount(final AccountHolder holder, final double balance) {
+        this.balance = balance;
+        this.accountHolder = holder;
+    }
+
+    @Override
+    public final AccountHolder getAccountHolder() {
+        return this.accountHolder;
+    }
+
+    @Override
+    public final double getBalance() {
+        return this.balance;
+    }
+
+    protected final void updateBalance(final double amount) {
+        this.balance += amount;
+    }
+
+    protected abstract double getWithdrawFee();
+    protected abstract double getDepositFee();
+
+    @Override
+    public final void deposit(int usrID, double amount) {
+        if (checkUser(usrID)) {
+            this.balance = this.balance + amount - getDepositFee();
+        }
+    }
+
+    @Override
+    public final void withdraw(int usrID, double amount) {
+        if (checkUser(usrID) && isWithdrawAllowed(amount)) {
+            this.balance = this.balance + amount - getWithdrawFee();
+        }
+    }
+
+    private boolean checkUser(final int id) {
+        return this.accountHolder.getId() == id;
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount - getWithdrawFee();
+    }
+}
